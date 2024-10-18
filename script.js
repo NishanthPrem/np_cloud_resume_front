081eb831df534f7dd8e7fbc580be29f09b1286b6
Nishanth Prem <nishanthprem8@gmail.com>
2024-10-17 17:55:57 -0700

    Removing the stage name.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiGatewayUrl = "https://kdyymm92fc.execute-api.us-west-1.amazonaws.com/$default";
+const apiGatewayUrl = "https://kdyymm92fc.execute-api.us-west-1.amazonaws.com";
 
 // Function to fetch visitor count
 function fetchVisitorCount() {
