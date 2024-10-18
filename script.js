611cadd26dfc5f28420cfc95a7fd33cf2dd71248
Nishanth Prem <nishanthprem8@gmail.com>
2024-10-18 14:25:48 -0700

    Updated Code.

--- script.js
+++ script.js
@@ -2,39 +2,38 @@ const apiGatewayUrl = "https://kdyymm92fc.execute-api.us-west-1.amazonaws.com/";
 
 // Function to trigger API Gateway and get visitor count
 function fetchVisitorCount() {
-
     // Check if the visitor count has already been fetched
-    const hasVisited = localStorage.getItem('hasVisited');
+    const visitorCount = localStorage.getItem('visitorCount');
 
-    if (!hasVisited) {
+    if (!visitorCount) {
         fetch(apiGatewayUrl, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({})
+            body: JSON.stringify({}) // Sending an empty body as no input is needed
         })
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok: ' + response.statusText);
             }
-            return response.json();
+            return response.json(); // Parse the JSON response
         })
         .then(data => {
-            const visitorCount = data.visitorCount;
+            const newVisitorCount = data.visitorCount; // Extract the count directly
 
             // Update the visitor count in the DOM
-            document.getElementById('visitorCount').textContent = visitorCount;
+            document.getElementById('visitorCount').textContent = newVisitorCount;
 
-            // Set the flag in local storage to indicate that the visitor count has been fetched
-            localStorage.setItem('hasVisited', 'true');
+            // Store the visitor count in local storage
+            localStorage.setItem('visitorCount', newVisitorCount);
         })
         .catch((error) => {
             console.error('Error fetching visitor count:', error);
             document.getElementById('visitorCount').textContent = 'Error';
         });
     } else {
-        // If the count has already been fetched, last known count
+        // If the count has already been fetched, display the stored count
         document.getElementById('visitorCount').textContent = visitorCount;
     }
 }
