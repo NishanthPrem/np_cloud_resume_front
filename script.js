f7cfb1ae13e6e2448faadd2ac3043485a50322da
Nishanth Prem <nishanthprem8@gmail.com>
2024-10-17 17:21:46 -0700

    Calling the fetchVisitCount function.

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
-const apiGatewayUrl = "https://kdyymm92fc.execute-api.us-west-1.amazonaws.com";
+const apiGatewayUrl = "https://kdyymm92fc.execute-api.us-west-1.amazonaws.com/visitor-count";
 
-// Trigger the API Gateway to get the visitor count
-fetch(apiGatewayUrl, {
-    method: 'POST', // Use GET if you're just retrieving data
-    headers: {
-        'Content-Type': 'application/json'
+    // Function to fetch visitor count
+    function fetchVisitorCount() {
+        fetch(apiGatewayUrl, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok: ' + response.statusText);
+            }
+            return response.json();
+        })
+        .then(data => {
+            const bodyString = data.body; // "visitorCount: 15"
+            const visitorCount = bodyString.match(/\d+/)[0]; // Extract the number
+            document.getElementById('visitorCount').textContent = visitorCount;
+        })
+        .catch((error) => {
+            console.error('Error fetching visitor count:', error);
+            document.getElementById('visitorCount').textContent = 'Error';
+        });
     }
-})
-.then(response => {
-    if (!response.ok) {
-        throw new Error('Network response was not ok: ' + response.statusText);
-    }
-    return response.json(); // Assuming the API returns JSON
-})
-.then(data => {
-        // The body contains a string, so we need to parse it
-        const bodyString = data.body;
-        const visitorCount = bodyString.match(/\d+/)[0];
 
-        // Update the visitor count in the DOM
-        document.getElementById('visitorCount').textContent = visitorCount;
-})
-.catch((error) => {
-    console.error('Error fetching visitor count:', error);
-    document.getElementById('visitorCount').textContent = 'Error'; // Show an error message in case of failure
-});
+    // Invoke the fetchVisitorCount function when the DOM is fully loaded
+    document.addEventListener('DOMContentLoaded', (event) => {
+        fetchVisitorCount();
+    });
